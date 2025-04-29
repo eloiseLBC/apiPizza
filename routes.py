65b4bc4b2d5f7360a203d8245fb0091d9e6ff073
eloiseLBC <eloise.leblanc@hotmail.com>
2025-04-29 15:39:29 +0200

    :rocket: WIP 7 - test images

--- routes.py
+++ routes.py
@@ -14,28 +14,10 @@ def register_routes(app):
         pizzas = Pizza.query.all()
         result = []
         for pizza in pizzas:
-            pizza_data = {
-                'id': pizza.id,
-                'name': pizza.name,
-                'ingredients': pizza.ingredients,
-                'image_url': pizza.image_url,
-                'price': pizza.price,
-                'tag': pizza.tag,
-                'categorie': pizza.categorie,
-                'features': json.loads(pizza.features) if pizza.features else {}
-            }
-            result.append(pizza_data), 200
+            result.append(pizza.serialize()), 200
         return jsonify(result)
 
     @app.route('/pizzas/<int:pizza_id>', methods=['GET'])
     def get_pizza(pizza_id):
         pizza = Pizza.query.get_or_404(pizza_id)
-        pizza_data = {
-                'id': pizza.id,
-                'name': pizza.name,
-                'ingredients': pizza.ingredients,
-                'image_url': pizza.image_url,
-                'price': pizza.price,
-                'features': json.loads(pizza.features) if pizza.features else {}
-            }
-        return jsonify(pizza_data), 200
+        return jsonify(pizza.serialize()), 200
